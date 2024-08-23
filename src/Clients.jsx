a167f5ad86fa668b432594b4fd6bfea21e3be701
Eng Bushra Jaber <jaberbushra828@gmail.com>
2024-08-23 19:38:16 +0300

    first commit

---
+++ src/Clients.jsx
@@ -0,0 +1,8 @@
+export default function Clients(){
+    return(
+    <section >
+    <h2>Clients</h2>
+
+  </section>
+);
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+     return (
+        <nav>
+          <a href="#employees">Employees</a>
+          <a href="#clients">Clients</a>
+        </nav>
+      );
+    };
+    
+  
+  
